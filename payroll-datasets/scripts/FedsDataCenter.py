fc7c2a68fdf94ed5c50138faee68c05b848a1143
Gideon Tong <gideon@gideontong.com>
2020-01-30 21:28:43 -0800

    Added sleep commands to prevent ban

--- payroll-datasets/scripts/FedsDataCenter.py
+++ payroll-datasets/scripts/FedsDataCenter.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from time import sleep
 
@@ -8,11 +9,21 @@ url_append = "&iDisplayLength=100"
 payload = {}
 headers= {}
 
-pages = 21083
+# pages = 21083
+pages = 3
 
-for i in range(21083):
-    print("Downloading page", i + 1, "of", pages)
+for i in range(pages):
+    print("Downloading page", i + 1, "of", pages,"..." ,end=" ")
     url = url_prepend + str(i) + url_append
     # response = requests.request("GET", url, headers=headers, data = payload)
+    print("Done!")
+    if (i + 1) % 1000 == 0:
+        print("Sleeping for a minute...")
+        sleep(5)
+        continue
+    if (i + 1) % 100 == 0:
+        print("Sleeping for a half minute...")
+        sleep(5)
+        continue
 
 # print(response.text.encode('utf8'))
