8f4fb0c08a3211447d145c4e8c1b3f29803b7f4e
Gideon Tong <gideon@gideontong.com>
2020-01-31 22:05:49 -0800

    Added header

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
+import Header from '../components/Header';
+
 return (
     <div>
-      <p>This is the about page</p>
+        <Header />
+        <p>This is the about page</p>
     </div>
-  );
+);
 }
