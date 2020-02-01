e96c9ee045e573beea6a7c3279ede9c190e0aeb3
Danial Khan <danialk1@berkeley.edu>
2020-02-01 09:48:53 -0800

    process basic information on male pay

--- parser.py
+++ parser.py
@@ -2,5 +2,9 @@ import csv
 def parseCSV(file_name):
     with open(file_name, 'r') as file_o_data:
         csv_data = csv.reader(file_o_data)#gives an iterable
-        for i in range(50):
-            print(next(csv_data))
+        processed_data = {'M':[],
+        'F':[]} #gender:annual salary
+        next(csv_data)
+        for datapoint in csv_data:
+            processed_data[datapoint[0]].append(datapoint[1])
+        print("the average male pay is", sum([int(float(i)) for i in processed_data['M']])/len(processed_data['M']))
