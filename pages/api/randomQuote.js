6d9cbe629e5c9390d8d666581ad593c57b58eeb5
Gideon Tong <gideon@gideontong.com>
2020-02-01 03:59:04 -0800

    Black magic deletion

--- pages/api/randomQuote.js
+++
@@ -1,6 +0,0 @@
-import quotes from '../../data/quotes.json';
-
-export default (req, res) => {
-    const quote = quotes[Math.floor(Math.random() * quotes.length)];
-    res.status(200).json(quote);
-};
