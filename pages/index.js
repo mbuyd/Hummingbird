d8818356e65762daa9aac3a0dfcba07abfc0cf79
Gideon Tong <gideon@gideontong.com>
2020-01-31 23:09:04 -0800

    Alright working quote with lambda functions and APIs

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,43 @@
-import Layout from '../components/Layout';
-import Link from 'next/link';
+import useSWR from 'swr';
 
-const PostLink = props => (
-    <li>
-        <Link href="/p/[id]" as={`/p/${props.id}`}>
-            <a>{props.id}</a>
-        </Link>
-    </li>
-);
+function fetcher(url) {
+  return fetch(url).then(r => r.json());
+}
 
 export default function Index() {
-    return (
-        <Layout>
-            <p>Hello Next.js</p>
-            <h1>My Blog</h1>
-            <ul>
-                <PostLink id="hello-nextjs" />
-                <PostLink id="learn-nextjs" />
-                <PostLink id="deploy-nextjs" />
-            </ul>
-        </Layout>
-    );
+  const { data, error } = useSWR('/api/randomQuote', fetcher);
+  // The following line has optional chaining, added in Next.js v9.1.5,
+  // is the same as `data && data.author`
+  const author = data?.author;
+  let quote = data?.quote;
+
+  if (!data) quote = 'Loading...';
+  if (error) quote = 'Failed to fetch the quote.';
+
+  return (
+    <main className="center">
+      <div className="quote">{quote}</div>
+      {author && <span className="author">- {author}</span>}
+
+      <style jsx>{`
+        main {
+          width: 90%;
+          max-width: 900px;
+          margin: 300px auto;
+          text-align: center;
+        }
+        .quote {
+          font-family: cursive;
+          color: #e243de;
+          font-size: 24px;
+          padding-bottom: 10px;
+        }
+        .author {
+          font-family: sans-serif;
+          color: #559834;
+          font-size: 20px;
+        }
+      `}</style>
+    </main>
+  );
 }
