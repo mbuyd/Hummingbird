87e89d1da9dd0560003bf79f9fb079581f93f764
Danial Khan <danialk1@berkeley.edu>
2020-02-02 05:18:36 -0800

    add p vals to frontt

--- lib/dataHandler.py
+++ lib/dataHandler.py
@@ -176,8 +176,10 @@ def main():
     # t, p = stats.ttest_ind(maleSalary, femaleSalary)
     # print("t and p:", t, p)
     allT = findAllT(race, gender, job, year, salary)
-    p_val_g= findAllT["gender"][1]
-    p_val_race= min([allT[key] for key in allT[race]])
+    print(allT)
+    p_val_g= allT["gender"][2][1]
+    p_val_race= min([allT['race'][key] for key in allT['race']][1])
+    print("p vals", p_val_g, p_val_race)
     # tVal = search_disparity(argumentList[0],  DataSections.GENDER, Gender.MALE.value, Gender.FEMALE.value)
     # comprehensive_data_analysis = complete_data_analysis(argumentList[0])
 
