a270fd3ef8d7a8987bc4effb5fefb98e26a0097d
Brian Lam <40176373+Brooseidon@users.noreply.github.com>
2020-02-01 18:40:21 -0800

    in process

--- controllers/dashboard.py
+++ controllers/dashboard.py
@@ -1,10 +1,22 @@
 from flask import Blueprint, render_template, abort
-
+from lib.dataHandler import *
 dashboard = Blueprint('dashboard', __name__,
                         template_folder='templates')
 
 @dashboard.route('/dashboard')
 def show():
+    return render_template('pages/dashboard.html',
+        size = 500, 
+        mfRatio = 99,
+        meanTc = 5000000,
+        jobCount = 5)
+
+@dashboard.route('/dashboard/<filename>')
+def filenameShow(filename):
+    print(filename)
+    print(parse(filename))
+    
+        
     return render_template('pages/dashboard.html',
         size = 500, 
         mfRatio = 99,
